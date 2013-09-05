8330ee350b77d69bc43264ea23eff10d306f7d54
Jim Bosch <jbosch@astro.princeton.edu>
2013-09-05 17:31:08 +0900

    Better plots for tractor-based profiles.

--- python/lsst/shapelet/tractor.py
+++ python/lsst/shapelet/tractor.py
@@ -36,6 +36,46 @@ except ImportError:
 
 import lsst.shapelet
 
+class sdss(object):
+    """Namespace-only class for the softened/truncated exponential and de Vaucouleur
+    profiles used in SDSS Photo model fits)."""
+
+    DEFAC = -7.66925
+    DEVOUT = 8.0
+    DEVCUT = 7.0
+
+    EXPFAC = -1.67835
+    EXPOUT = 4.0
+    EXPCUT = 3.0
+
+    @classmethod
+    def dev(cls, r):
+        """Truncated de Vaucouleur - copied/translated from SDSS Photo package
+
+        Expected input is a NumPy array of radius values.
+        """
+        p = numpy.exp(cls.DEFAC * ((r**2 + 0.0004)**0.125 - 1.0))
+        big = r > cls.DEVCUT
+        scr = (r[big] - cls.DEVCUT) / (cls.DEVOUT - cls.DEVCUT)
+        scr = 1.0 - scr**2
+        p[big] *= scr*scr
+        p[r > cls.DEVOUT] = 0.0
+        return p
+
+    @classmethod
+    def exp(cls, r):
+        """Truncated exponential - copied/translated from SDSS from SDSS Photo package
+
+        Expected input is a NumPy array of radius values.
+        """
+        p = numpy.exp(cls.EXPFAC * (r - 1.0))
+        big = r > cls.EXPCUT
+        scr = (r[big] - cls.EXPCUT) / (cls.EXPOUT - cls.EXPCUT);
+        scr = 1.0 - scr**2
+        p[big] *= scr * scr
+        p[r > cls.EXPOUT] = 0.0
+        return p
+
 def loadParameters(profile, nComponents, maxRadius=None):
     """Load the parameters of a multi-Gaussian profile
 
@@ -91,7 +131,7 @@ def loadBasis(profile, nComponents, maxRadius=None):
         basis.addComponent(radius, 0, matrix)
     return basis
 
-def plotRadial(basis):
+def evaluateRadial(basis, r, sbNormalize=False, doComponents=False):
     """Plot a single-element MultiShapeletBasis as a radial profile.
     """
     from matplotlib import pyplot
@@ -99,9 +139,87 @@ def plotRadial(basis):
     coefficients = numpy.ones(1, dtype=float)
     msf = basis.makeFunction(ellipse, coefficients)
     ev = msf.evaluate()
-    r = numpy.linspace(0, 8, 1000)
-    z = numpy.zeros(r.shape, dtype=float)
-    for n, x in enumerate(r):
-        z[n] = ev(x, 0.0)
-    pyplot.plot(r, z)
-    return r, z
+    n = 1
+    if doComponents:
+        n += len(msf.getElements())
+    z = numpy.zeros((n,) + r.shape, dtype=float)
+    for j, x in enumerate(r):
+        z[0,j] = ev(x, 0.0)
+    if doComponents:
+        for i, sf in enumerate(msf.getElements()):
+            evc = sf.evaluate()
+            for j, x in enumerate(r):
+                z[i+1,j] = evc(x, 0.0)
+    if sbNormalize:
+        z /= ev(1.0, 0.0)
+    return z
+
+def plotSuite(doComponents=False):
+    from matplotlib import pyplot
+    fig = pyplot.figure(figsize=(9,4.7))
+    axes = numpy.zeros((2,4), dtype=object)
+    r1 = numpy.logspace(-3, 0, 1000, base=10)
+    r2 = numpy.linspace(1, 10, 1000)
+    r = [r1, r2]
+    for i in range(2):
+        for j in range(4):
+            axes[i,j] = fig.add_subplot(2, 4, i*4+j+1)
+    basis = {name: loadBasis(name, nComponents=8) for name in ("exp", "lux", "dev", "luv")}
+    z = numpy.zeros((2,4), dtype=object)
+    colors = ("k", "g", "b", "r")
+    fig.subplots_adjust(wspace=0.025, hspace=0.025, bottom=0.15, left=0.1, right=0.98, top=0.92)
+    centers = [None, None]
+    for i in range(2):
+        for j in range(0,4,2):
+            bbox0 = axes[i,j].get_position()
+            bbox1 = axes[i,j+1].get_position()
+            bbox1.x0 = bbox0.x1 - 0.06
+            bbox0.x1 = bbox1.x0
+            centers[j/2] = 0.5*(bbox0.x0 + bbox1.x1)
+            axes[i,j].set_position(bbox0)
+            axes[i,j+1].set_position(bbox1)
+    for j in range(0,2):
+        z[0,j] = [evaluateRadial(basis[k], r[j], sbNormalize=True, doComponents=doComponents)
+                  for k in ("exp", "lux")]
+        z[0,j][0:0] = [numpy.exp(-1.67834699*(r[j] - 1.0))[numpy.newaxis,:],
+                       sdss.exp(r[j])[numpy.newaxis,:]]
+        z[0,j+2] = [evaluateRadial(basis[k], r[j], sbNormalize=True, doComponents=doComponents)
+                    for k in ("dev", "luv")]
+        z[0,j+2][0:0] = [numpy.exp(-7.66924944*(r[j]**0.25 - 1.0))[numpy.newaxis,:],
+                         sdss.dev(r[j])[numpy.newaxis,:]]
+    methodNames = [["loglog", "semilogy"], ["semilogx", "plot"]]
+    for j in range(0,4):
+        z[1,j] = [(z[0,j][0][0,:] - z[0,j][i][0,:])/z[0,j][0][0,:] for i in range(0,4)]
+        handles = []
+        method0 = getattr(axes[0,j], methodNames[0][j%2])
+        method1 = getattr(axes[1,j], methodNames[1][j%2])
+        for k in range(4):
+            z0 = z[0,j][k]
+            handles.append(method0(r[j%2], z0[0,:], color=colors[k])[0])
+            if doComponents:
+                for l in range(1, z0.shape[0]):
+                    method0(r[j%2], z0[l,:], color=colors[k], alpha=0.25)
+            method1(r[j%2], z[1,j][k], color=colors[k])
+        axes[0,j].set_xticklabels([])
+        axes[0,j].set_ylim(1E-6, 1E3)
+        axes[1,j].set_ylim(-0.2, 1.0)
+    for i, label in enumerate(("profile", "relative error")):
+        axes[i,0].set_ylabel(label)
+        for t in axes[i,0].get_yticklabels():
+            t.set_fontsize(11)
+    for j in range(1,4):
+        axes[0,j].set_yticklabels([])
+        axes[1,j].set_yticklabels([])
+    xticks = [['$\\mathdefault{10^{%d}}$' % i for i in range(-3,1)],
+               [str(i) for i in range(1,11)]]
+    xticks[0][-1] = ""
+    xticks[1][-1] = ""
+    for j in range(0,4):
+        axes[1,j].set_xticklabels(xticks[j%2])
+        for t in axes[1,j].get_xticklabels():
+            t.set_fontsize(11)
+    fig.legend(handles, ["exp/dev", "lux/luv", "approx exp/dev", "approx lux/luv"],
+               loc='lower center', ncol=4)
+    fig.text(centers[0], 0.95, "exponential", ha='center', weight='bold')
+    fig.text(centers[1], 0.95, "de Vaucouleur", ha='center', weight='bold')
+    return fig, axes
