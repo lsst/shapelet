5a8cc26d34ba59ca0c8a3c502bc71dea61a34c62
Pim Schellart <P.Schellart@princeton.edu>
2017-01-18 14:47:01 -0500

    Wrap with pybind11 instead of swig

---
+++ python/lsst/shapelet/matrixBuilder.cc
@@ -0,0 +1,143 @@
+/*
+ * LSST Data Management System
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ * See the COPYRIGHT file
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <https://www.lsstcorp.org/LegalNotices/>.
+ */
+#include "pybind11/pybind11.h"
+
+#include "numpy/arrayobject.h"
+#include "ndarray/pybind11.h"
+
+#include "lsst/shapelet/MatrixBuilder.h"
+#include "lsst/shapelet/MultiShapeletBasis.h"
+
+namespace py = pybind11;
+using namespace pybind11::literals;
+
+namespace lsst {
+namespace shapelet {
+
+namespace {
+
+template <typename T>
+py::class_<MatrixBuilder<T>, std::shared_ptr<MatrixBuilder<T>>> declareMatrixBuilder(
+    py::module &mod, std::string const &suffix) {
+    using Class = MatrixBuilder<T>;
+
+    py::class_<Class, std::shared_ptr<Class>> cls(mod, ("MatrixBuilder" + suffix).c_str());
+
+    cls.def(py::init<ndarray::Array<T const, 1, 1> const &, ndarray::Array<T const, 1, 1> const &, int>(),
+            "x"_a, "y"_a, "order"_a);
+    cls.def(py::init<ndarray::Array<T const, 1, 1> const &, ndarray::Array<T const, 1, 1> const &, int,
+                     ShapeletFunction const &>(),
+            "x"_a, "y"_a, "order"_a, "psf"_a);
+    cls.def(py::init<ndarray::Array<T const, 1, 1> const &, ndarray::Array<T const, 1, 1> const &,
+                     MultiShapeletBasis const &>(),
+            "x"_a, "y"_a, "basis"_a);
+    cls.def(py::init<ndarray::Array<T const, 1, 1> const &, ndarray::Array<T const, 1, 1> const &,
+                     MultiShapeletBasis const &, MultiShapeletFunction const &>(),
+            "x"_a, "y"_a, "basis"_a, "psf"_a);
+
+    cls.def("getDataSize", &Class::getDataSize);
+    cls.def("getBasisSize", &Class::getBasisSize);
+    cls.def("allocateOutput", &Class::allocateOutput);
+
+    cls.def("__call__",
+            (void (Class::*)(ndarray::Array<T, 2, -1> const &, afw::geom::ellipses::Ellipse const &) const) &
+                Class::operator());
+    cls.def("__call__", (ndarray::Array<T, 2, -2> (Class::*)(afw::geom::ellipses::Ellipse const &) const) &
+                            Class::operator());
+
+    return cls;
+}
+
+template <typename T>
+py::class_<MatrixBuilderWorkspace<T>, std::shared_ptr<MatrixBuilderWorkspace<T>>>
+declareMatrixBuilderWorkspace(py::module &mod, std::string const &suffix) {
+    using Class = MatrixBuilderWorkspace<T>;
+
+    py::class_<Class, std::shared_ptr<Class>> cls(mod, ("MatrixBuilderWorkspace" + suffix).c_str());
+
+    cls.def(py::init<int>(), "size"_a);
+    cls.def(py::init<Class const &>(), "other"_a);
+
+    cls.def("getRemaining", &Class::getRemaining);
+
+    return cls;
+}
+
+template <typename T>
+py::class_<MatrixBuilderFactory<T>, std::shared_ptr<MatrixBuilderFactory<T>>> declareMatrixBuilderFactory(
+    py::module &mod, std::string const &suffix) {
+    using Class = MatrixBuilderFactory<T>;
+
+    py::class_<Class, std::shared_ptr<Class>> cls(mod, ("MatrixBuilderFactory" + suffix).c_str());
+
+    cls.def(py::init<ndarray::Array<T const, 1, 1> const &, ndarray::Array<T const, 1, 1> const &, int>(),
+            "x"_a, "y"_a, "order"_a);
+    cls.def(py::init<ndarray::Array<T const, 1, 1> const &, ndarray::Array<T const, 1, 1> const &, int,
+                     ShapeletFunction const &>(),
+            "x"_a, "y"_a, "order"_a, "psf"_a);
+    cls.def(py::init<ndarray::Array<T const, 1, 1> const &, ndarray::Array<T const, 1, 1> const &,
+                     MultiShapeletBasis const &>(),
+            "x"_a, "y"_a, "basis"_a);
+    cls.def(py::init<ndarray::Array<T const, 1, 1> const &, ndarray::Array<T const, 1, 1> const &,
+                     MultiShapeletBasis const &, MultiShapeletFunction const &>(),
+            "x"_a, "y"_a, "basis"_a, "psf"_a);
+
+    cls.def("__call__", (MatrixBuilder<T> (Class::*)() const) & Class::operator(), py::is_operator());
+    cls.def("__call__", (MatrixBuilder<T> (Class::*)(typename Class::Workspace &) const) & Class::operator(),
+            py::is_operator(), "workspace"_a);
+
+    cls.def("getDataSize", &Class::getDataSize);
+    cls.def("getBasisSize", &Class::getBasisSize);
+    cls.def("computeWorkspace", &Class::computeWorkspace);
+
+    return cls;
+}
+
+template <typename T>
+void declareMatrixBuilderTemplates(py::module &mod, std::string const &suffix) {
+    auto clsMatrixBuilder = declareMatrixBuilder<T>(mod, suffix);
+    auto clsMatrixBuilderWorkspace = declareMatrixBuilderWorkspace<T>(mod, suffix);
+    auto clsMatrixBuilderFactory = declareMatrixBuilderFactory<T>(mod, suffix);
+
+    clsMatrixBuilder.attr("Workspace") = clsMatrixBuilderWorkspace;
+    clsMatrixBuilder.attr("Factory") = clsMatrixBuilderFactory;
+
+    clsMatrixBuilderFactory.attr("Workspace") = clsMatrixBuilderWorkspace;
+    clsMatrixBuilderFactory.attr("Builder") = clsMatrixBuilder;
+}
+}
+
+PYBIND11_PLUGIN(_matrixBuilder) {
+    py::module mod("_matrixBuilder");
+
+    if (_import_array() < 0) {
+        PyErr_SetString(PyExc_ImportError, "numpy.core.multiarray failed to import");
+        return nullptr;
+    }
+
+    declareMatrixBuilderTemplates<float>(mod, "F");
+    declareMatrixBuilderTemplates<double>(mod, "D");
+
+    return mod.ptr();
+}
+}
+}  // lsst::shapelet
---
+++ python/lsst/shapelet/multiShapeletFunction.cc
@@ -0,0 +1,113 @@
+/*
+ * LSST Data Management System
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ * See the COPYRIGHT file
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <https://www.lsstcorp.org/LegalNotices/>.
+ */
+#include "pybind11/pybind11.h"
+#include "pybind11/stl.h"
+
+#include "numpy/arrayobject.h"
+#include "ndarray/pybind11.h"
+
+#include "lsst/shapelet/MultiShapeletFunction.h"
+
+namespace py = pybind11;
+using namespace pybind11::literals;
+
+namespace lsst {
+namespace shapelet {
+
+namespace {
+
+template <typename PyClass>
+void declareMultiShapeletFunctionMembers(PyClass &cls) {
+    using Class = MultiShapeletFunction;
+
+    cls.def(py::init<>());
+    cls.def(py::init<Class const &>());
+    cls.def(py::init<typename Class::ComponentList const &>());
+    cls.def(py::init<ShapeletFunction const &>());
+
+    cls.def("getComponents", [](Class const &self) {
+        auto &components = self.getComponents();
+
+        py::tuple t(components.size());
+        for (size_t i = 0; i < components.size(); ++i) {
+            t[i] = py::cast(components[i]);
+        }
+
+        return t;
+    });
+    cls.def("addComponent",
+            [](Class &self, typename Class::Component const &c) { self.getComponents().push_back(c); });
+    cls.def("normalize", &Class::normalize, "value"_a = 1.0);
+    cls.def("shiftInPlace", &Class::shiftInPlace);
+    cls.def("transformInPlace", &Class::transformInPlace);
+    cls.def("convolve", (Class (Class::*)(ShapeletFunction const &) const) & Class::convolve);
+    cls.def("convolve", (Class (Class::*)(Class const &) const) & Class::convolve);
+    cls.def("evaluate", &Class::evaluate);
+}
+
+template <typename PyClass>
+void declareMultiShapeletFunctionEvaluatorMembers(PyClass &cls) {
+    using Class = MultiShapeletFunctionEvaluator;
+
+    cls.def(py::init<MultiShapeletFunction const &>());
+
+    cls.def("__call__", (double (Class::*)(double, double) const) & Class::operator());
+    cls.def("__call__", (double (Class::*)(afw::geom::Point2D const &) const) & Class::operator());
+    cls.def("__call__", (double (Class::*)(afw::geom::Extent2D const &) const) & Class::operator());
+    cls.def("__call__",
+            (ndarray::Array<double, 1, 1> (Class::*)(ndarray::Array<double const, 1> const &,
+                                                     ndarray::Array<double const, 1> const &) const) &
+                Class::operator());
+
+    cls.def("addToImage",
+            (void (Class::*)(ndarray::Array<double, 2, 1> const &, afw::geom::Point2I const &) const) &
+                Class::addToImage,
+            "array"_a, "xy0"_a = afw::geom::Point2I());
+    cls.def("addToImage", (void (Class::*)(afw::image::Image<double> &) const) & Class::addToImage,
+            "image"_a);
+
+    cls.def("integrate", &Class::integrate);
+    cls.def("computeMoments", &Class::computeMoments);
+    cls.def("update", &Class::update);
+}
+}
+
+PYBIND11_PLUGIN(_multiShapeletFunction) {
+    py::module mod("_multiShapeletFunction");
+
+    if (_import_array() < 0) {
+        PyErr_SetString(PyExc_ImportError, "numpy.core.multiarray failed to import");
+        return nullptr;
+    }
+
+    py::class_<MultiShapeletFunction, std::shared_ptr<MultiShapeletFunction>> clsMultiShapeletFunction(
+        mod, "MultiShapeletFunction");
+    py::class_<MultiShapeletFunctionEvaluator, std::shared_ptr<MultiShapeletFunctionEvaluator>>
+        clsMultiShapeletFunctionEvaluator(mod, "MultiShapeletFunctionEvaluator");
+
+    declareMultiShapeletFunctionMembers(clsMultiShapeletFunction);
+    declareMultiShapeletFunctionEvaluatorMembers(clsMultiShapeletFunctionEvaluator);
+
+    return mod.ptr();
+}
+}
+}  // lsst::shapelet
