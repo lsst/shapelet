lsst/shapelet

commit c17fe67f384d80489135f4620f219637706beff6

RadialProfile class to make persisted bases available in C++
Also updated tests and visualization code in tractor.py to
make use of the new class.

python/lsst/shapelet/tractor.py
@@ -29,113 +29,51 @@ package for more information.
 
 import numpy
 import os
+import re
+import warnings
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 
-import lsst.shapelet
+from .shapeletLib import *
 
-class sdss(object):
-    """Namespace-only class for the softened/truncated exponential and de Vaucouleur
-    profiles used in SDSS Photo model fits)."""
+def registerRadialProfiles():
+    """Register the pickled profiles in the data directory with the RadialProfile singleton registry.
 
-    DEVFAC = -7.66925
-    DEVOUT = 8.0
-    DEVCUT = 7.0
-
-    EXPFAC = -1.67835
-    EXPOUT = 4.0
-    EXPCUT = 3.0
-
-    @classmethod
-    def dev(cls, r):
-        """Truncated de Vaucouleur - copied/translated from SDSS Photo package
-
-        Expected input is a NumPy array of radius values, output is a NumPy array
-        of the same shape containing the profile.
-
-        Normalized to unit surface brightness at the half-ligth radius.
-        """
-        p = numpy.exp(cls.DEVFAC * ((r**2 + 0.0004)**0.125 - 1.0))
-        big = r > cls.DEVCUT
-        scr = (r[big] - cls.DEVCUT) / (cls.DEVOUT - cls.DEVCUT)
-        scr = 1.0 - scr**2
-        p[big] *= scr*scr
-        p[r > cls.DEVOUT] = 0.0
-        return p
-
-    @classmethod
-    def exp(cls, r):
-        """Truncated exponential - copied/translated from SDSS from SDSS Photo package
-
-        Expected input is a NumPy array of radius values, output is a NumPy array
-        of the same shape containing the profile.
-
-        Normalized to unit surface brightness at the half-ligth radius.
-        """
-        p = numpy.exp(cls.EXPFAC * (r - 1.0))
-        big = r > cls.EXPCUT
-        scr = (r[big] - cls.EXPCUT) / (cls.EXPOUT - cls.EXPCUT);
-        scr = 1.0 - scr**2
-        p[big] *= scr * scr
-        p[r > cls.EXPOUT] = 0.0
-        return p
-
-def loadParameters(profile, nComponents, maxRadius=None):
-    """Load the parameters of a multi-Gaussian profile
-
-    @param[in]   profile       Name of the ideal profile being approximated; usually one of
-                               'exp', 'dev', 'ser2', 'ser3', 'lux', or 'luv'.
-    @param[in]   nComponents   Number of Gaussians in the approximation.
-    @param[in]   maxRadius     Maximum radius at which the multi-Gaussian approximated was optimized, in
-                               units of the half-light radius.  Defaults to 8 for all profiles except 'lux',
-                               which is only defined to 4.
-
-    @return a tuple of (amplitudes, variances), each of which are NumPy arrays of length nComponents,
-            containing the relative flux and variance of the components
-
-    The returned profiles are defined such that the half-light radius of the profile and integrated flux
-    are both unity.
+    This should only be called at import time by this module; it's only a function to avoid polluting
+    the module namespace with all the local variables used here.
     """
-    if maxRadius is None:
-        if profile == 'lux':
-            maxRadius = 4
-        else:
-            maxRadius = 8
-    name = "%s_K%02d_MR%02d.pickle" % (profile, nComponents, maxRadius)
-    path = os.path.join(os.environ["SHAPELET_DIR"], "data", name)
-    with open(path, 'r') as stream:
-        array = pickle.load(stream)
-    amplitudes = array[:nComponents]
-    amplitudes /= amplitudes.sum()
-    variances = array[nComponents:]
-    if amplitudes.shape != (nComponents,) or variances.shape != (nComponents,):
-        raise ValueError("Unknown format for pickled profile file %s" % name)
-    return amplitudes, variances
-
-def loadBasis(profile, nComponents, maxRadius=None):
-    """Load the parameters of a multi-Gaussian profile into a single-element MultiShapeletBasis.
-
-    @param[in]   profile       Name of the ideal profile being approximated; usually one of
-                               'exp', 'dev', 'ser2', 'ser3', 'lux', or 'luv'.
-    @param[in]   nComponents   Number of Gaussians in the approximation.
-    @param[in]   maxRadius     Maximum radius at which the multi-Gaussian approximated was optimized, in
-                               units of the half-light radius.  Defaults to 8 for all profiles except 'lux',
-                               which is only defined to 4.
-
-    @return a MultiShapeletBasis object
-
-    The returned profiles are defined such that the half-light radius of the profile and integrated flux
-    are both unity.
-    """
-    amplitudes, variances = loadParameters(profile, nComponents, maxRadius)
-    basis = lsst.shapelet.MultiShapeletBasis(1)
-    for amplitude, variance in zip(amplitudes, variances):
-        radius = variance**0.5
-        matrix = numpy.array([[amplitude / lsst.shapelet.ShapeletFunction.FLUX_FACTOR]], dtype=float)
-        basis.addComponent(radius, 0, matrix)
-    return basis
+    dataDir = os.path.join(os.environ["SHAPELET_DIR"], "data")
+    regex = re.compile(r"([a-z]+\d?)_K(\d+)_MR(\d+)\.pickle")
+    for filename in os.listdir(dataDir):
+        match = regex.match(filename)
+        if not match: continue
+        name = match.group(1)
+        nComponents = int(match.group(2))
+        maxRadius = int(match.group(3))
+        try:
+            profile = RadialProfile.get(name)
+        except lsst.pex.exceptions.Exception:
+            warnings.warn("No C++ profile for multi-Gaussian pickle file '%s'" % filename)
+            continue
+        with open(os.path.join(dataDir, filename), 'r') as stream:
+            array = pickle.load(stream)
+        amplitudes = array[:nComponents]
+        amplitudes /= amplitudes.sum()
+        variances = array[nComponents:]
+        if amplitudes.shape != (nComponents,) or variances.shape != (nComponents,):
+            warnings.warn("Unknown format for multi-Gaussian pickle file '%s'" % filename)
+            continue
+        basis = MultiShapeletBasis(1)
+        for amplitude, variance in zip(amplitudes, variances):
+            radius = variance**0.5
+            matrix = numpy.array([[amplitude / ShapeletFunction.FLUX_FACTOR]], dtype=float)
+            basis.addComponent(radius, 0, matrix)
+        profile.registerBasis(basis, nComponents, maxRadius)
+# We register all the profiles at module import time, to allow C++ code to access all available profiles
+# without having to later call Python code to unpickle them.
+registerRadialProfiles()
 
 def evaluateRadial(basis, r, sbNormalize=False, doComponents=False):
     """Plot a single-element MultiShapeletBasis as a radial profile.
@@ -160,6 +98,27 @@ def evaluateRadial(basis, r, sbNormalize=False, doComponents=False):
         z /= ev(1.0, 0.0)
     return z
 
+def integrateNormalizedFluxes(maxRadius=20.0, nSteps=5000):
+    """!
+    After normalizing by surface brightness at r=1 r_e, integrate the profiles to compare
+    relative fluxes between the true profiles and their approximations.
+
+    @param[in] maxRadius   Maximum radius to integrate the profile, in units of r_e.
+    @param[in] nSteps      Number of concrete points at which to evaluate the profile to
+                           do the integration (we just use the trapezoidal rule).
+    """
+    radii = numpy.linspace(0.0, maxRadius, nSteps)
+    profiles = {name: RadialProfile.get(name) for name in ("exp", "lux", "dev", "luv",
+                                                           "ser2", "ser3", "ser5")}
+    evaluated = {}
+    for name, profile in profiles.iteritems():
+        evaluated[name] = profile.evaluate(radii)
+        basis = profile.getBasis(8)
+        evaluated["g" + name] = evaluateRadial(basis, radii, sbNormalize=True, doComponents=False)[0,:]
+    fluxes = {name: numpy.trapz(z*radii, radii) for name, z in evaluated.iteritems()}
+    return fluxes
+
+
 def plotSuite(doComponents=False):
     """Plot all the profiles defined in this module together: true exp and dev, the SDSS softended/truncated
     lux and luv, and the multi-Gaussian approximations to all of these.
@@ -177,9 +136,10 @@ def plotSuite(doComponents=False):
     r = [r1, r2]
     for i in range(2):
         for j in range(4):
-            axes[i, j] = fig.add_subplot(2, 4, i*4+j+1)
-    basis = {name: loadBasis(name, nComponents=8) for name in ("exp", "lux", "dev", "luv")}
-    y = numpy.zeros((2, 4), dtype=object)
+            axes[i,j] = fig.add_subplot(2, 4, i*4+j+1)
+    profiles = {name: RadialProfile.get(name) for name in ("exp", "lux", "dev", "luv")}
+    basis = {name: profiles[name].getBasis(8) for name in profiles}
+    z = numpy.zeros((2,4), dtype=object)
     colors = ("k", "g", "b", "r")
     fig.subplots_adjust(wspace=0.025, hspace=0.025, bottom=0.15, left=0.1, right=0.98, top=0.92)
     centers = [None, None]
@@ -190,17 +150,15 @@ def plotSuite(doComponents=False):
             bbox1.x0 = bbox0.x1 - 0.06
             bbox0.x1 = bbox1.x0
             centers[j/2] = 0.5*(bbox0.x0 + bbox1.x1)
-            axes[i, j].set_position(bbox0)
-            axes[i, j+1].set_position(bbox1)
-    for j in range(0, 2):  # grid columns: 0=small radii (log axis), 1=large radii (linear axis)
-        y[0, j] = [evaluateRadial(basis[profile], r[j], sbNormalize=True, doComponents=doComponents)
-                  for profile in ("exp", "lux")]
-        y[0, j][0:0] = [numpy.exp(sdss.EXPFAC*(r[j] - 1.0))[numpy.newaxis, :],
-                       sdss.exp(r[j])[numpy.newaxis, :]]
-        y[0, j+2] = [evaluateRadial(basis[profile], r[j], sbNormalize=True, doComponents=doComponents)
-                    for profile in ("dev", "luv")]
-        y[0, j+2][0:0] = [numpy.exp(sdss.DEVFAC*(r[j]**0.25 - 1.0))[numpy.newaxis, :],
-                         sdss.dev(r[j])[numpy.newaxis, :]]
+            axes[i,j].set_position(bbox0)
+            axes[i,j+1].set_position(bbox1)
+    for j in range(0,2):
+        z[0,j] = [evaluateRadial(basis[k], r[j], sbNormalize=True, doComponents=doComponents)
+                  for k in ("exp", "lux")]
+        z[0,j][0:0] = [profiles[k].evaluate(r[j])[numpy.newaxis,:] for k in ("exp", "lux")]
+        z[0,j+2] = [evaluateRadial(basis[k], r[j], sbNormalize=True, doComponents=doComponents)
+                    for k in ("dev", "luv")]
+        z[0,j+2][0:0] = [profiles[k].evaluate(r[j])[numpy.newaxis,:] for k in ("dev", "luv")]
     methodNames = [["loglog", "semilogy"], ["semilogx", "plot"]]
     for j in range(0, 4): # grid columns
         y[1, j] = [(y[0, j][0][0, :] - y[0, j][i][0, :])/y[0, j][0][0, :] for i in range(0, 4)]

include/lsst/shapelet.h
@@ -39,5 +39,6 @@
 #include "lsst/shapelet/GaussHermiteProjection.h"
 #include "lsst/shapelet/MultiShapeletBasis.h"
 #include "lsst/shapelet/MatrixBuilder.h"
+#include "lsst/shapelet/RadialProfile.h"
 
 #endif // !defined(LSST_AFW_MATH_SHAPELETS_H)

include/lsst/shapelet/RadialProfile.h
@@ -1,0 +1,131 @@
+// -*- lsst-c++ -*-
+/*
+ * LSST Data Management System
+ * Copyright 2008-2014 LSST Corporation.
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <http://www.lsstcorp.org/LegalNotices/>.
+ */
+
+#ifndef LSST_SHAPELET_RadialProfile_h_INCLUDED
+#define LSST_SHAPELET_RadialProfile_h_INCLUDED
+
+#include <map>
+
+#include "lsst/shapelet/MultiShapeletBasis.h"
+
+namespace lsst { namespace shapelet {
+
+/**
+ *  Registry and utility class for multi-Gaussian approximations to radial profiles.
+ *
+ *  RadialProfile provides C++ access to the saved multi-Gaussian approximations stored in the pickle
+ *  files in the data subdirectory of the shapelet package, by maintaining a singleton registry of these
+ *  that is populated at import time by tractor.py.  It also provides access to the true (exact) profiles,
+ *  (mostly useful for educational/diagnostic purposes) and information about the relationship between
+ *  different radii for this profile (see getMomentsRadiusFactor()).
+ *
+ *  Each RadialProfile object represents a particular "true" profile, and can hold multiple multi-Gaussian
+ *  (or multi-Shapelet) approximations with different degrees of fidelity, as controlled by the number of
+ *  components in the approximation and the maximum radius at which the approximation was fit.  For more
+ *  information about these approximations, see tractor.py and references therein.  All RadialProfiles
+ *  are defined in units of the half-light radius of the true profile, even for profiles for which
+ *  this is not the radius typically used.
+ *
+ *  Several predefined subclasses of RadialProfile are defined privately, and can be accessed by name
+ *  through the static get() method:
+ *   - "gaussian": a single Gaussian profile
+ *   - "exp": Exponential profile (Sersic n=1)
+ *   - "ser2": Sersic profile with n=2
+ *   - "ser3": Sersic profile with n=3
+ *   - "dev": de Vaucouleur profile (Sersic n=4)
+ *   - "ser5": Sersic profile with n=5
+ *   - "lux": SDSS truncated Exponential profile
+ *   - "luv": SDSS truncated and softened de Vaucouleur profile
+ */
+class RadialProfile {
+public:
+
+    /**
+     *  @brief Return a predefined radial profile given its name.
+     *
+     *  New RadialProfile classes are registered when their constructor is invoked, so all subclasses
+     *  should only be instantiated once (at module scope).
+     */
+    static RadialProfile & get(std::string const & name);
+
+    /// Return the name of the profile.
+    std::string const getName() const { return _name; }
+
+    /**
+     *  @brief Evaluate the profile at the given radius
+     *
+     *  Radius is given in units of half-light radius, and the profile is normalized to 1 at r=1.
+     */
+    virtual double evaluate(double r) const = 0;
+
+    /**
+     *  @brief Evaluate the profile at the given radius (vectorized).
+     *
+     *  Radius is given in units of half-light radius, and the profile is normalized to 1 at r=1.
+     */
+    ndarray::Array<double,1,1> evaluate(ndarray::Array<double const,1,1> const & r) const;
+
+    /// Return the 2nd-moment radius in units of the half-light radius.
+    double getMomentsRadiusFactor() const { return _momentsRadiusFactor; }
+
+    /**
+     *  @brief Return a multi-Gaussian approximation to the radial profile
+     *
+     *  Unlike the evaluate() method, the returned basis is normalized that the integrated
+     *  flux is equal to the (single) amplitude.
+     *
+     *  @param[in] nComponents   Number of Gaussians used to approximate the profile.
+     *  @param[in] maxRadius     Maximum radius used in fitting the approximation.  Passing 0
+     *                           selects the default for that profile.
+     *
+     *  Gaussian approximations are computed in advance and persisted, and are usually loaded
+     *  and registered with the RadialProfile object in Python at module-import time.
+     *
+     *  Throws NotFoundError if the a basis with the given combination of nComponents and
+     *  maxRadius has not been added to the RadialProfile.
+     */
+    PTR(MultiShapeletBasis) getBasis(int nComponents, int maxRadius=0) const;
+
+    /**
+     *  @brief Register a basis with the profile, making it available to callers of getBasis().
+     */
+    void registerBasis(PTR(MultiShapeletBasis) basis, int nComponents, int maxRadius);
+
+protected:
+
+    explicit RadialProfile(std::string const & name, int defaultMaxRadius);
+
+    double _momentsRadiusFactor;
+
+private:
+
+    typedef std::map<std::pair<int,int>,PTR(MultiShapeletBasis)> BasisRegistry;
+
+    std::string const _name;
+    int _defaultMaxRadius;
+    BasisRegistry _basisRegistry;
+};
+
+}} // namespace lsst::shapelet
+
+#endif // !LSST_SHAPELET_RadialProfile_h_INCLUDED

python/lsst/shapelet/__init__.py
@@ -21,6 +21,7 @@
 #
 
 from shapeletLib import *
+from . import tractor
 from lsst.afw.geom.ellipses import Quadrupole as EllipseCore
 
 class IndexGenerator(object):

python/lsst/shapelet/shapeletLib.i
@@ -55,6 +55,9 @@ Python interface to lsst::shapelet classes and functions
 %init %{
     import_array();
 %}
+%pythoncode %{
+    import numpy
+%}
 
 %include "ndarray.i"
 
@@ -135,3 +138,14 @@ def __reduce__(self):
 %}
 
 }
+
+%extend lsst::shapelet::RadialProfile {
+    %feature("shadow") evaluate %{
+    def evaluate(self, r):
+        if isinstance(r, numpy.ndarray):
+            return $action(self, r.ravel()).reshape(r.shape)
+        else:
+            return $action(self, r)
+    %}
+}
+%include "lsst/shapelet/RadialProfile.h"

src/RadialProfile.cc
@@ -1,0 +1,184 @@
+// -*- lsst-c++ -*-
+/*
+ * LSST Data Management System
+ * Copyright 2008-2014 LSST Corporation.
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <http://www.lsstcorp.org/LegalNotices/>.
+ */
+
+#include "boost/make_shared.hpp"
+#include "boost/math/special_functions/gamma.hpp"
+
+#include "lsst/shapelet/RadialProfile.h"
+
+namespace lsst { namespace shapelet {
+
+namespace {
+
+typedef std::map<std::string,RadialProfile*> RadialProfileRegistry;
+
+RadialProfileRegistry & getRadialProfileRegistry() {
+    static RadialProfileRegistry registry;
+    return registry;
+}
+
+
+class SersicRadialProfile : public RadialProfile {
+public:
+
+    virtual double evaluate(double r) const {
+        return std::exp(-_kappa*(std::pow(r, 1.0 / _n) - 1.0));
+    }
+
+    explicit SersicRadialProfile(std::string const & name, double n, int defaultMaxRadius=8) :
+        RadialProfile(name, defaultMaxRadius), _n(n), _kappa(boost::math::gamma_q_inv(2*n, 0.5))
+    {
+        _momentsRadiusFactor = std::pow(_kappa, -n) * std::sqrt(boost::math::tgamma_ratio(4*n, 2*n)/2);
+    }
+
+private:
+    double _n;
+    double _kappa;
+};
+
+SersicRadialProfile expProfile("exp", 1.0);
+SersicRadialProfile ser2Profile("ser2", 2.0);
+SersicRadialProfile ser3Profile("ser3", 3.0);
+SersicRadialProfile devProfile("dev", 4.0);
+SersicRadialProfile ser5Profile("ser5", 5.0);
+
+class GaussianRadialProfile : public SersicRadialProfile {
+public:
+
+    explicit GaussianRadialProfile() :
+        SersicRadialProfile("gaussian", 0.5, 0)
+    {
+        PTR(MultiShapeletBasis) basis = boost::make_shared<MultiShapeletBasis>(1);
+        ndarray::Array<double,2,2> matrix = ndarray::allocate(1,1);
+        matrix.deep() = 0.5 / std::sqrt(afw::geom::PI);
+        basis->addComponent(_momentsRadiusFactor, 0, matrix);
+        registerBasis(basis, 1, 0);
+    }
+
+};
+
+GaussianRadialProfile gaussianProfilex;
+
+// Truncated exp profile used in SDSS pipeline
+class LuxRadialProfile : public RadialProfile {
+public:
+
+    virtual double evaluate(double r) const {
+        // formula and constants taken from SDSS Photo code
+        static double const EXPFAC = -1.67835;
+        static double const EXPOUT = 4.0;
+        static double const EXPCUT = 3.0;
+        if (r > EXPOUT) return 0.0;
+        double p = std::exp(EXPFAC * (r - 1.0));
+        if (r > EXPCUT) {
+            double s = (r - EXPCUT) / (EXPOUT - EXPCUT);
+            s = 1.0 - s*s;
+            p *= s*s;
+        }
+        return p;
+    }
+
+    LuxRadialProfile() : RadialProfile("lux", 4) {
+        _momentsRadiusFactor = 0.95473; // computed in Mathematica
+    }
+};
+
+LuxRadialProfile luxProfile;
+
+// Softened, truncated de Vaucouleur profile used in SDSS pipeline
+class LuvRadialProfile : public RadialProfile {
+public:
+
+    virtual double evaluate(double r) const {
+        // formula and constants taken from SDSS Photo code
+        static double const DEFAC = -7.66925;
+        static double const DEVOUT = 8.0;
+        static double const DEVCUT = 7.0;
+        if (r > DEVOUT) return 0.0;
+        double p = std::exp(DEFAC*(std::pow(r*r + 0.0004, 0.125) - 1.0));
+        if (r > DEVCUT) {
+            double s = (r - DEVCUT) / (DEVOUT - DEVCUT);
+            s = 1.0 - s*s;
+            p *= s*s;
+        }
+        return p;
+    }
+
+    LuvRadialProfile() : RadialProfile("luv", 8) {
+        _momentsRadiusFactor = 1.57016; // computed in Mathematica
+    }
+};
+
+LuvRadialProfile luvProfile;
+
+} // anonymous
+
+RadialProfile::RadialProfile(std::string const & name, int defaultMaxRadius) :
+    _momentsRadiusFactor(std::numeric_limits<double>::quiet_NaN()),
+    _name(name),
+    _defaultMaxRadius(defaultMaxRadius)
+{
+    getRadialProfileRegistry()[_name] = this;
+}
+
+RadialProfile & RadialProfile::get(std::string const & name) {
+    RadialProfileRegistry::const_iterator i = getRadialProfileRegistry().find(name);
+    if (i == getRadialProfileRegistry().end()) {
+        throw LSST_EXCEPT(
+            pex::exceptions::NotFoundError,
+            (boost::format("RadialProfile with name '%s' not found") % name).str()
+        );
+    }
+    return *i->second;
+}
+
+ndarray::Array<double,1,1> RadialProfile::evaluate(ndarray::Array<double const,1,1> const & r) const {
+    ndarray::Array<double,1,1> p = ndarray::allocate(r.getSize<0>());
+    for (int i = 0, n = r.getSize<0>(); i < n; ++i) {
+        p[i] = evaluate(r[i]);
+    }
+    return p;
+}
+
+PTR(MultiShapeletBasis) RadialProfile::getBasis(int nComponents, int maxRadius) const {
+    if (maxRadius == 0) maxRadius = _defaultMaxRadius;
+    BasisRegistry::const_iterator i = _basisRegistry.find(std::make_pair(nComponents, maxRadius));
+    if (i == _basisRegistry.end()) {
+        throw LSST_EXCEPT(
+            pex::exceptions::NotFoundError,
+            (boost::format("Basis for profile '%s' with nComponents=%d and maxRadius=%d not present")
+             % _name % nComponents % maxRadius).str()
+        );
+    }
+    // Ideally, we'd return a const ptr instead of copying, but because Swig casts away the
+    // constness, that's not sufficiently safe.
+    return boost::make_shared<MultiShapeletBasis>(*i->second);
+}
+
+void RadialProfile::registerBasis(PTR(MultiShapeletBasis) basis, int nComponents, int maxRadius) {
+    _basisRegistry[std::make_pair(nComponents, maxRadius)] = basis;
+}
+
+
+
+}} // namespace lsst::shapelet

tests/profiles.py
@@ -2,7 +2,7 @@
 
 #
 # LSST Data Management System
-# Copyright 2008-2013 LSST Corporation.
+# Copyright 2008-2014 LSST Corporation.
 #
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
@@ -41,8 +41,6 @@ E1 = 0.3
 E2 = -0.2
 PROFILES = [
     ("exp", 9, 8),
-    ("ser2", 9, 8),
-    ("ser3", 9, 8),
     ("dev", 9, 8),
     ]
 
@@ -50,23 +48,50 @@ CHECK_COMPONENT_IMAGES = False
 
 class ProfileTestCase(lsst.shapelet.tests.ShapeletTestCase):
 
-    def testProfiles(self):
+    def testRadii(self):
+        """Check RadialProfile definitions of moments and half-light radii.
+        """
+        s = numpy.linspace(-20.0, 20.0, 1000)
+        x, y = numpy.meshgrid(s, s)
+        r = (x**2 + y**2)**0.5
+        dxdy = (s[1] - s[0])**2
+        for name in ["gaussian", "exp", "ser2", "luv", "lux"]:
+            profile = lsst.shapelet.RadialProfile.get(name)
+            z = profile.evaluate(r) * dxdy
+            # lux and luv don't use the true half-light radius; instead they use the half-light radius
+            # of the exp and dev profiles they approximate
+            if not name.startswith("lu"):
+                self.assertClose(z[r < 1].sum(), 0.5*z.sum(), rtol=0.01)
+            # lhs of this comparison is the moments radius (using a sum approximation to the integral)
+            self.assertClose(((z*x**2).sum() / z.sum())**0.5, profile.getMomentsRadiusFactor(), rtol=0.01)
+
+    def testGaussian(self):
+        """Test that the Gaussian profile's shapelet 'approximation' is actually exact.
+        """
+        profile = lsst.shapelet.RadialProfile.get("gaussian")
+        r = numpy.linspace(0.0, 4.0, 100)
+        z1 = profile.evaluate(r)
+        basis = profile.getBasis(1)
+        z2 = lsst.shapelet.tractor.evaluateRadial(basis, r, sbNormalize=True)[0,:]
+        self.assertClose(z1, z2, rtol=1E-8)
+
+    def testShapeletApproximations(self):
         psf0 = lsst.shapelet.ShapeletFunction(0, lsst.shapelet.HERMITE, PSF_SIGMA)
         psf0.getCoefficients()[:] = 1.0 / lsst.shapelet.ShapeletFunction.FLUX_FACTOR
         psf = lsst.shapelet.MultiShapeletFunction()
         psf.getComponents().push_back(psf0)
         psf.normalize()
         ellipse = el.Separable[el.Distortion, el.DeterminantRadius](E1, E2, GALAXY_RADIUS)
-        for profile, nComponents, maxRadius in PROFILES:
+        for name, nComponents, maxRadius in PROFILES:
             # check1 is the multi-Gaussian approximation, as convolved and evaluated by GalSim,
-            check1 = lsst.afw.image.ImageD(os.path.join("tests", "data", profile + "-approx.fits")).getArray()
+            check1 = lsst.afw.image.ImageD(os.path.join("tests", "data", name + "-approx.fits")).getArray()
             xc = check1.shape[1] // 2
             yc = check1.shape[0] // 2
             xb = numpy.arange(check1.shape[1], dtype=float) - xc
             yb = numpy.arange(check1.shape[0], dtype=float) - yc
             xg, yg = numpy.meshgrid(xb, yb)
 
-            basis = lsst.shapelet.tractor.loadBasis(profile, nComponents, maxRadius)
+            basis = lsst.shapelet.RadialProfile.get(name).getBasis(nComponents, maxRadius)
             builder = lsst.shapelet.MatrixBuilderD.Factory(xg.ravel(), yg.ravel(), basis, psf)()
             image1 = numpy.zeros(check1.shape, dtype=float)
             matrix = image1.reshape(check1.size, 1)
@@ -79,13 +104,13 @@ class ProfileTestCase(lsst.shapelet.tests.ShapeletTestCase):
             msf.evaluate().addToImage(lsst.afw.image.ImageD(image2, False))
             self.assertClose(check1, image2, plotOnFailure=True, rtol=5E-5, relTo=check1.max())
 
-            if profile == 'exp':
+            if name == 'exp':
                 # check2 is the exact profile, again by GalSim.
                 # We only check exp against the exact profile.  The other approximations are less
                 # accurate, and we only really need to test one.  The real measure of whether these
                 # profiles are good enough is more complicated than what we can do in a unit test.
                 check2 = lsst.afw.image.ImageD(
-                    os.path.join("tests", "data", profile + "-exact.fits")
+                    os.path.join("tests", "data", name + "-exact.fits")
                 ).getArray()
                 self.assertClose(check2, image1, plotOnFailure=True, rtol=1E-3, relTo=check2.max())
 
@@ -95,7 +120,7 @@ class ProfileTestCase(lsst.shapelet.tests.ShapeletTestCase):
                 # it's disabled by default.
                 for n, sf in enumerate(msf.getComponents()):
                     check = lsst.afw.image.ImageD(
-                        os.path.join("tests", "data", "%s-approx-%0d.fits" % (profile, n))
+                        os.path.join("tests", "data", "%s-approx-%0d.fits" % (name, n))
                     ).getArray()
                     image = numpy.zeros(check1.shape, dtype=float)
                     sf.evaluate().addToImage(lsst.afw.image.ImageD(image, False))

commit 949a348a18b369f16dd1787c06cbb0f1c0a2c78e

Fix variable name y->z
Bug introduced in c17fe67f384d80489135f4620f219637706beff6

python/lsst/shapelet/tractor.py
@@ -173,17 +173,17 @@ def plotSuite(doComponents=False):
         z[0, j+2][0:0] = [profiles[k].evaluate(r[j])[numpy.newaxis, :] for k in ("dev", "luv")]
     methodNames = [["loglog", "semilogy"], ["semilogx", "plot"]]
     for j in range(0, 4):  # grid columns
-        y[1, j] = [(y[0, j][0][0, :] - y[0, j][i][0, :])/y[0, j][0][0, :] for i in range(0, 4)]
+        z[1, j] = [(z[0, j][0][0, :] - z[0, j][i][0, :])/z[0, j][0][0, :] for i in range(0, 4)]
         handles = []
         method0 = getattr(axes[0, j], methodNames[0][j%2])
         method1 = getattr(axes[1, j], methodNames[1][j%2])
         for k in range(4):
-            y0 = y[0, j][k]
+            y0 = z[0, j][k]
             handles.append(method0(r[j%2], y0[0, :], color=colors[k])[0])
             if doComponents:
                 for l in range(1, y0.shape[0]):
                     method0(r[j%2], y0[l, :], color=colors[k], alpha=0.25)
-            method1(r[j%2], y[1, j][k], color=colors[k])
+            method1(r[j%2], z[1, j][k], color=colors[k])
         axes[0, j].set_xticklabels([])
         axes[0, j].set_ylim(1E-6, 1E3)
         axes[1, j].set_ylim(-0.2, 1.0)