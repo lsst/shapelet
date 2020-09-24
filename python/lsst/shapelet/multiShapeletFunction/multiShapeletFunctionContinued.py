271efcd7c6b55d81dfe907e30059ccc0d0424749
Brian Van Klaveren <bvan@slac.stanford.edu>
2020-09-24 13:37:50 -0700

    Update F811 annotations for py 3.7+3.8

--- python/lsst/shapelet/multiShapeletFunction/multiShapeletFunctionContinued.py
+++ python/lsst/shapelet/multiShapeletFunction/multiShapeletFunctionContinued.py
@@ -5,7 +5,7 @@ from lsst.utils import continueClass
 __all__ = []
 
 
-@continueClass  # noqa F811
-class MultiShapeletFunction:
+@continueClass  # noqa: F811 (FIXME: remove for py 3.8+)
+class MultiShapeletFunction:  # noqa: F811
     def __reduce__(self):
         return (MultiShapeletFunction, (list(self.getComponents()),))
