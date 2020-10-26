1d22010451440d8ada192eb209578e0033cf592c
Brian Van Klaveren <bvan@slac.stanford.edu>
2020-10-26 12:27:33 -0700

    Update F811 annotations for py 3.7+3.8

--- python/lsst/shapelet/shapeletFunction/shapeletFunctionContinued.py
+++ python/lsst/shapelet/shapeletFunction/shapeletFunctionContinued.py
@@ -5,8 +5,8 @@ from lsst.utils import continueClass
 __all__ = []
 
 
-@continueClass  # noqa
-class ShapeletFunction:
+@continueClass  # noqa: F811 (FIXME: remove for py 3.8+)
+class ShapeletFunction:  # noqa: F811
     def __reduce__(self):
         return (ShapeletFunction, (self.getOrder(), self.getBasisType(),
                                    self.getEllipse(), self.getCoefficients()))
